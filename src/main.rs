407d3c9ddae4963c5579b28335b1767c67747b2f
jo53289 <jo53289@student.uni-lj.si>
2026-05-29 15:23:31 +0200

    dodala dashboard.html in style.css

--- src/main.rs
+++ src/main.rs
@@ -98,6 +98,7 @@ use settlemate_rust::database::connect;
 use settlemate_rust::handlers::groups::list_group_members;
 use settlemate_rust::handlers::index::index;
 use settlemate_rust::handlers::users::{create_user_handler, list_users};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
@@ -116,6 +117,7 @@ async fn main() {
         .route("/users", get(list_users).post(create_user_handler))
         // Primer in-memory endpoint-a: glej `src/handlers/groups.rs`.
         .route("/groups/{id}/members", get(list_group_members))
+        .nest_service("/static", ServeDir::new("static"))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
